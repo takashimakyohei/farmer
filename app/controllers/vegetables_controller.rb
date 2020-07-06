239db3a6d091487016d191df9f7754f85b207efb
takashimakyohei <hagi.suzu@icloud.com>
2020-07-06 23:05:52 +0900

    vegetablesコントローラーのnew,index,show,editの記述

--- app/controllers/vegetables_controller.rb
+++ app/controllers/vegetables_controller.rb
@@ -1,13 +1,51 @@
 class VegetablesController < ApplicationController
   def index
+    @vegetables = Vegetable.all
   end
 
   def show
+    @vegetable = Vegetable.find(params[:id])
   end
 
   def new
+    @vegetable = Vegetable.new
+  end
+
+  def create
+    @vegetable = Vegetable.new(vegetable_params)
+    @vegetable.shop_id = current_shop.id
+    if @vegetable.save
+      redirect_to vegetable_path(@vegetable), notice: "投稿に成功しました"
+    else
+      render :new
+    end
   end
 
   def edit
+    @vegetable = Vegetable.find(params[:id])
+    if @vegetable.shop_id != current_shop.id
+      redirect_to vegetables_path, alert: "不正なアクセスです"
+    end
+  end
+
+  def update
+    @vegetable = Vegetable.find(params[:id])
+    if @vegetable.update(vegetable_params)
+      redirect_to vegetable_path(@vegetable), notice: "編集に成功しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    vegetable = Vegetable.find(params[:id])
+    vegetable.destroy
+    redirect_to vegetables_path
+  end
+
+  private
+
+  def vegetable_params
+    params.require(:vegetable).permit(:title, :description, :image, :prefecture)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'vegetables/index'
-  get 'vegetables/show'
-  get 'vegetables/new'
-  get 'vegetables/edit'
   root to: "homes#index"
   get "homes/login"
   devise_for :shops
   devise_for :users
+  resources :vegetables
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
