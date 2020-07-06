047884b1a9ac4c1add7fd7e04fabbe2e0f24e6aa
takashimakyohei <hagi.suzu@icloud.com>
2020-07-06 21:13:55 +0900

    deviseで受け付けるカラム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    if resource_class == User
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :image])
+    elsif resource_class == Shop
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :address, :image, :prefecture])
+    else
+      super
+    end
+  end
 end
