0aa53c17eb497a8f3b21e10347f6874259a4417a
takashimakyohei <hagi.suzu@icloud.com>
2020-07-06 21:19:53 +0900

    トップページからuser,shopのログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/index'
+  root to: "homes#index"
   devise_for :shops
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
