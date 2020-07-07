e4d8b683c9e4a0338fb8b18867d8cb21da057961
takashimakyohei <hagi.suzu@icloud.com>
2020-07-07 17:19:33 +0900

    いいね機能に関するモデル間のアソシエーション定義

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,6 @@
 class Favorite < ApplicationRecord
+  belongs_to :user
+  belongs_to :vegetable
+
+  validates_uniqueness_of :vetetable_id, scope: :user_id
 end
--- app/models/vegetable.rb
+++ app/models/vegetable.rb
@@ -1,4 +1,5 @@
 class Vegetable < ApplicationRecord
   belongs_to :shop
+  has_many :favorites, dependent: :destroy
   mount_uploader :image, ImageUploader
 end
