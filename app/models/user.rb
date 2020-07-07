ab608e991d971cc73d9162859f06c48883ac037f
takashimakyohei <hagi.suzu@icloud.com>
2020-07-07 22:39:30 +0900

    コメントモデルに関するアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   mount_uploader :image, ImageUploader
 end
--- app/models/vegetable.rb
+++ app/models/vegetable.rb
@@ -1,5 +1,7 @@
 class Vegetable < ApplicationRecord
   belongs_to :shop
   has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   mount_uploader :image, ImageUploader
 end
